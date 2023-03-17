7c3dc3ef961b5fde6a528ab6444333a945a90ff3
haileighB <90584516+haileighB@users.noreply.github.com>
2023-03-17 12:17:32 -0300

    fix errors

--- main.tf
+++ main.tf
@@ -35,6 +35,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -87,7 +88,7 @@ module "blog_sg" {
   version = "4.17.1"
   name = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
